415a48957b9cc026f0aa5f0677bb21d84a3f5d5a
MAT-2 <mpcasey30@gmail.com>
2023-09-27 11:32:39 -0500

    Created an array called options. Expanded my if statement to make sure that the choices made are strictly made for the ones in the options array. Otherwise, if no correct option was made, we get an alert to refresh the page to do it again.

--- script.js
+++ script.js
@@ -2,11 +2,26 @@
 
 var confirmPlay = confirm("Would you like to play Rock, Paper, Scissors?");
 
+var options = ["R", "P", "S", "r", "p", "s"];
+
 if (confirmPlay === true) {
   var choice = prompt("Please choose R, P, or S:");
+  if (choice === options[0]) {
+    console.log(choice);
+  } else if (choice === options[1]) {
+    console.log(choice);
+  } else if (choice === options[2]) {
+    console.log(choice);
+  } else if (choice === options[3]) {
+    console.log(choice);
+  } else if (choice === options[4]) {
+    console.log(choice);
+  } else if (choice === options[5]) {
+    console.log(choice);
+  } else if (choice !== options) {
+    alert("You did not pick the right criteria. Refresh and try again!");
+  }
 } else {
   var thankYou = document.createTextNode("Thank you for playing!");
   document.body.appendChild(thankYou);
 }
-
-console.log(choice);
